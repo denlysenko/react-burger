56f9426d3bb98e2dea4c4dd580faf62945b0afa4
den.lysenko <den.l@april33.com>
2018-04-26 18:00:31 +0300

    redux setup

---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  ingredients: null,
+  totalPrice: 4
+};
+
+const reducer = (state = initialState, action) {
+  switch () {}
+
+  return state;
+}
+
+export default reducer;
